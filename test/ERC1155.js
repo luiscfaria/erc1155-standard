5d6d750583047d5b210b19ed242ca5bc6f9a0a36
Luis <luiscarlossfaria@gmail.com>
2022-12-12 23:35:45 -0300

    Added more simple testing

--- test/ERC1155.js
+++ test/ERC1155.js
@@ -14,19 +14,49 @@ describe("Faria Token", function () {
     addr3 = users[3];
   });
 
+  beforeEach(async () => {
+    FariaToken = await ethers.getContractFactory("FariaToken");
+    FariaToken = await FariaToken.deploy();
+    await FariaToken.deployed();
+  });
+
   describe("Deployment", function () {
-    it("Should Deploy Contract", async function () {
-      FariaToken = await ethers.getContractFactory("FariaToken");
-      FariaToken = await FariaToken.deploy();
-      await FariaToken.deployed();
+    it("Should setURI as URI Setter", async function () {
+      await expect(FariaToken.setURI("baseURI")).not.to.be.reverted;
+    });
+
+    it("Should not setURI if not URI Setter", async function () {
+      await expect(FariaToken.connect(addr1).setURI("baseURI")).to.be.revertedWith(
+        "AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x7804d923f43a17d325d77e781528e0793b2edd9890ab45fc64efd7b4b427744c"
+      );
     });
 
     it("Should Mint as Minter", async function () {
-      FariaToken = await ethers.getContractFactory("FariaToken");
-      FariaToken = await FariaToken.deploy();
-      await FariaToken.deployed();
+      await expect(FariaToken.mint(addr1.address, 1, 1, "0x")).not.to.be.reverted;
+    });
 
-      expect(FariaToken.mint(addr1.address, 1, 1, ""));
+    it("Should Revert if not Minter", async function () {
+      await expect(
+        FariaToken.connect(addr1).mint(addr2.address, 1, 1, "0x")
+      ).to.be.revertedWith(
+        "AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"
+      );
+    });
+
+    it("Should MintBatch as Minter", async function () {
+      const ids = [1, 2, 3];
+      const amounts = [2, 2, 3]
+      await expect(FariaToken.mintBatch(addr1.address, ids, amounts, "0x")).not.to.be.reverted;
+    });
+
+    it("Should Revert if not Minter", async function () {
+      const ids = [1, 2, 3];
+      const amounts = [2, 2, 3]
+      await expect(
+        FariaToken.connect(addr1).mint(addr2.address, ids, amounts, "0x")
+      ).to.be.revertedWith(
+        "AccessControl: account 0x70997970c51812dc3a010c7d01b50e0d17dc79c8 is missing role 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"
+      );
     });
   });
 });
